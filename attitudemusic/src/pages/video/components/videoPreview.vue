3d3f65f4177781214cee6be1498e3d2902485a18
huanghanzeng <3505563308@qq.com>
2020-01-17 19:25:28 +0800

    瞎改

--- attitudemusic/src/pages/video/components/videoPreview.vue
+++ attitudemusic/src/pages/video/components/videoPreview.vue
@@ -21,10 +21,12 @@ export default {
   data() {
     return {
       spinning: false,
-      title: ''
+      title: '',
+      video: ''
     }
   },
   created() {
+    this.video = this.videoSrc
     this.getVideo()
   },
   methods: {
@@ -32,16 +34,16 @@ export default {
       if(this.$store.state.user.videos) {  
           let params = {
             _id: this.$store.state.user._id,
-            video: this.videoSrc
+            video: this.video
           }
           let res = await getVideo(params)
           if(res.status === 200) {
             const blob = new Blob([res.data], {type: 'application/mp4'});
             const href = window.URL.createObjectURL(blob);
-            this.videoSrc = href;
+            this.video = href;
             new Player({
               id: 'preview',
-              url: this.videoSrc,
+              url: this.video,
               fluid: true,
               videoInit: true,
               playbackRate: [0.5, 0.75, 1, 1.5, 2],
